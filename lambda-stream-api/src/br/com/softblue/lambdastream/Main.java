e673621b5f706883768ce5d2ae97754f94802169
Mario <mario.martins@sonda.com>
2020-09-08 16:48:18 -0300

    7.4 (Prática): A Stream API aplicada em coleções do Java

--- lambda-stream-api/src/br/com/softblue/lambdastream/Main.java
+++ lambda-stream-api/src/br/com/softblue/lambdastream/Main.java
@@ -1,7 +1,6 @@
 package br.com.softblue.lambdastream;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
---
+++ lambda-stream-api/src/br/com/softblue/lambdastream/Main2.java
@@ -0,0 +1,23 @@
+package br.com.softblue.lambdastream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main2 {
+	
+	public static void main(String[] args) {
+		List<String> words = List.of("CARLOS", "JOSÉ", "JOÃO", "JOANA", "MARIA", "CELSO");
+		
+//		List<String> filtereWords = 
+		words.stream()
+			.filter(w -> w.startsWith("J"))
+			.map(w -> w.toLowerCase())
+			.forEach(w -> System.out.println(w));
+//			.collect(Collectors.toList());
+		
+//		filtereWords.stream().forEach(w -> System.out.println(w));
+//		filtereWords.forEach(w -> System.out.println(w));
+//		System.out.println(filtereWords);
+	}
+
+}
